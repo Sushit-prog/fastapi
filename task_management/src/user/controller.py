82a3de690e38bc380eb24158b7e06f1c8cdc4cca
Sushit-prog <pakrashys@gmail.com>
2026-09-15 23:55:25 +0530

    Update user authentication and settings

--- task_management/src/user/controller.py
+++ task_management/src/user/controller.py
@@ -1,14 +1,22 @@
-from fastapi import HTTPException
-from src.user.dtos import UserSchema
+from fastapi import HTTPException, status
+from src.user.dtos import UserSchema, LoginSchema
 from sqlalchemy.orm import Session
 from src.user.models import UserModel
 from pwdlib import PasswordHash
+from src.utils.settings import settings
+from datetime import datetime, timedelta
+import jwt 
+
 
 
 password_hash = PasswordHash.recommended()
 
+
 def get_password_hash(password):
   return password_hash.hash(password)
+
+def verify_password(plain_password, hashed_password):
+  return password_hash.verify(plain_password,hashed_password)
 
 def register(body:UserSchema, db:Session):
   
@@ -38,4 +46,16 @@ def register(body:UserSchema, db:Session):
   return new_user
 
 
-def login_user()
+def login_user(body:LoginSchema, db:Session):
+  user = db.query(UserModel).filter(UserModel.username==body.username).first()
+  if not user:
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You Entered wrong username")
+
+  if not verify_password(body.password, user.hash_password):
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You Entered wrong password")
+
+    exp_time = datetime.now() + timedelta(minutes=settings.EXP_TIME)
+
+  token = jwt.encode({"_id":user.id, "exp":exp_time},settings.SECRET_KEY, settings.ALGORITHM)
+  
+  return {"token":token}
